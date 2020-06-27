032755d2b5a104f1730241dc883af3ebec798130
Christopher Dutcher <cadutcher@icloud.com>
2020-06-27 10:50:03 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,7 +3,7 @@ def third_challenge
    :montague => {
       :patriarch => {
         :name => "Lord Montague",
-        :age => "53"  
+        :age => "53"
         },
       :matriarch => {
         :name => "Lady Montague",
@@ -15,7 +15,7 @@ def third_challenge
         :status => 'alive'
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet",
@@ -34,5 +34,5 @@ def third_challenge
    }
   }
 
-  
+
 end
