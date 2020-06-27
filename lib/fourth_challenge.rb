082b509c2a534ed2b20f7d46746196e78fc62269
Christopher Dutcher <cadutcher@icloud.com>
2020-06-27 10:58:03 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -14,9 +14,9 @@ def fourth_challenge
             :name => "Mercutio",
             :age => "18",
             :attitude => "hot-headed"
-          }  
+          }
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -36,6 +36,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
